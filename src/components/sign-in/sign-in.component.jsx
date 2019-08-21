ca543a547f6ce654d132c9037479fb750d896756
acty123 <johan.andres.garzon@correounivalle.edu.co>
2019-08-21 18:40:41 -0500

    add SignOut and styles for authentication with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -29,7 +29,7 @@ class SignIn extends React.Component {
     render(){
         return (
             <div className='sign-in'>
-                <h2>I alreadyhave an account </h2>
+                <h2>I already have an account </h2>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
@@ -49,12 +49,14 @@ class SignIn extends React.Component {
                         value={this.state.password} 
                         required 
                     />
-
-                    <CustomButton type='submit' >Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} >
-                        {' '}
-                        Sign in with Google{' '}
-                    </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit' >Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn >
+                            {' '}
+                            Sign in with Google{' '}
+                        </CustomButton>
+                    </div>
+                    
                 </form>
             </div>
         )
