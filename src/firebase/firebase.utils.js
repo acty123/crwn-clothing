62ef62a468486dacfee591d8b7eba2054bc84993
acty123 <johan.andres.garzon@correounivalle.edu.co>
2019-08-23 16:11:55 -0500

    firebase function to create users when they auth with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
     appId: "1:1004558762849:web:5678da02f029376c"
   };
 
+export const createUserProfileDocument = async (userAuth, addionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...addionalData
+        })
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+    return userRef;
+  };
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
